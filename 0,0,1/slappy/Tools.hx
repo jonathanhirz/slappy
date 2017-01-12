da97405965c8491db49f28bfb2a2d3356f18a9da
Jonathan Hirz <jonathanhirz@gmail.com>
2017-01-12 15:38:00 -0800

    initial, added Tools

---
+++ 0,0,1/slappy/Tools.hx
@@ -0,0 +1,13 @@
+package slappy;
+
+class Tools {
+
+    static inline public function BoolToInt(_bool:Bool):Int {
+        
+        if(_bool == true) return 1;
+        else if(_bool == false) return 0;
+        else return 0;
+
+    } //BoolToInt
+
+} //Tools
