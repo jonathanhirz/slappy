664d9b4570a94d0d6464433bc088326929a8c04c
Jonathan Hirz <jonathanhirz@gmail.com>
2017-01-21 13:57:21 -0800

    tiled_tile_has_property()

--- slappy/Bass.hx
+++ slappy/Bass.hx
@@ -1,5 +1,8 @@
 package slappy;
 
+import luxe.importers.tiled.*;
+import luxe.tilemaps.Tilemap;
+
 class Bass {
 
     // convert Bool (true/false) to Int (1/0)
@@ -11,10 +14,22 @@ class Bass {
 
     } //bool_to_int
 
-    // give it a position and a tilemap (and layer?), return the Tiled custom property of the tile
-    static inline public function tile_property_at_position() {
+    function tiled_tile_has_property( _tilemap:TiledMap, _layer:String, _position_x:Float, _position_y:Float, _property:String ) : Bool {
 
+            var tile_coordinates = _tilemap.tile_coord(_position_x, _position_y);
+            if(_tilemap.inside(Std.int(tile_coordinates.x), Std.int(tile_coordinates.y))) {
+                var current_tile_id : Int = _tilemap.tile_at_pos(_layer, _position_x, _position_y).id - 1;
+                for(tiled_tileset in _tilemap.tiledmap_data.tilesets) {
+                    for(tiled_tile in tiled_tileset.property_tiles) {
+                        if(current_tile_id == tiled_tile.id) {
+                            return tiled_tile.properties.exists(_property);
+                        }
+                    }
+                }
+            }
 
-    } //tile_property_at_position
+            return false;
+
+        } //tiled_tile_has_property
 
 } //Bass
