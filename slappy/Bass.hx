7bc2fb5af45909c8c554cc55893e7bd2b362267c
Jonathan Hirz <jonathanhirz@gmail.com>
2017-01-21 11:45:53 -0800

    change function name, block out property function

--- slappy/Bass.hx
+++ slappy/Bass.hx
@@ -3,12 +3,18 @@ package slappy;
 class Bass {
 
     // convert Bool (true/false) to Int (1/0)
-    static inline public function BoolToInt(_bool:Bool):Int {
+    static inline public function bool_to_int(_bool:Bool):Int {
         
         if(_bool == true) return 1;
         else if(_bool == false) return 0;
         else return 0;
 
-    } //BoolToInt
+    } //bool_to_int
+
+    // give it a position and a tilemap (and layer?), return the Tiled custom property of the tile
+    static inline public function tile_property_at_position() {
+
+
+    } //tile_property_at_position
 
 } //Bass
